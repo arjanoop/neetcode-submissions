a28cd6bf39709caf5211bc83cfc0671a11aa7003
Anoop Ranjan Jha <41595802+arjanoop@users.noreply.github.com>
2026-08-18 11:27:12 +0200

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.java
@@ -0,0 +1,15 @@
+// This one is better solution
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int gap = 0, i = 0;
+        for (i = 0; i < nums.length - gap;) {
+            nums[i] = nums[i + gap];
+            if (nums[i] == val) {
+                gap++;
+                continue;
+            }
+            i++;
+        }
+        return i;
+    }
+}
