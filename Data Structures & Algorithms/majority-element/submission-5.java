1e3ae3e23e0bad5d4b7f4168d64575f315096333
Anoop Ranjan Jha <41595802+arjanoop@users.noreply.github.com>
2026-08-19 15:44:00 +0200

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.java
@@ -0,0 +1,20 @@
+// Boyer-Moore Majority Voting Algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0], count = 0;
+
+        for (int n : nums) {
+            if (n == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = n;
+                    count++;
+                }
+            }
+        }
+
+        return candidate;
+    }
+}
